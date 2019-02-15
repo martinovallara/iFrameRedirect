4d627395f68668cec21b6e47e8603ba148faeee8
Martino Vallara <martino.vallara@xpeppers.com>
2019-02-15 19:43:36 +0100

    introduce ejs for dinamic views

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var path = require('path');
 var express = require('express');
 var request = require('request');
 var app = express();
-app.set('view engine', 'ejs');
+var morgan = require('morgan');
 
+app.set('view engine', 'ejs');
+app.use(morgan('combined'));
 app.use('/static', express.static('public'));
 app.use('/merchant-website', express.static('public/views'));
 app.use('/3ds-server/views', express.static('3ds-server/views'));
@@ -13,18 +15,28 @@ app.use('/acs', express.static('acs'));
 app.set('views', path.join(__dirname, '/public'));
 
 app.post('/3ds-server-gateway/init', function (req, res) {
-    request('http://localhost:3000/3ds-server/api/init', { pan: '123' }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        console.log(body.url);
-        console.log(body.explanation);
-    });
+    request({
+        url: 'http://localhost:3000/3ds-server/api/init',
+        method: 'POST',
+        json: {pan: '123'}
+      }, function(error, response, body){
+
+        console.log('response 3ds-server/init' + body);
+
+      });
+
+
     //res.sendFile(path.join(__dirname, '/3ds-server-gateway/views/init.html'));
-    res.render('3ds-server-gateway/init', {gdiUrl:'/3ds-server/views/frame.html'})
+    res.render('3ds-server-gateway/init', { gdiUrl: '/3ds-server/frame' })
 });
 
+app.get('/3ds-server/frame', function (req, res) {
+    res.render('3ds-server/frame', { gdiNotifyUrl: '/merchant-website/gdiNotify.html' })
+});
 
 app.post('/3ds-server/api/browserInformation', function (req, res) {
-    res.redirect('/static/gdiNotify.html');
+    //res.redirect('/static/gdiNotify.html');
+    res.sendStatus(200);
 });
 
 app.post('/3ds-server/api/verify', function (req, res) {
@@ -32,10 +44,11 @@ app.post('/3ds-server/api/verify', function (req, res) {
 });
 
 app.post('/3ds-server/api/init', function (req, res) {
+
     setTimeout(sendStatus, 1000);
 
     function sendStatus() {
-        res.status(200).send('http://localhost:3000/3ds-server/frame.html');
+        res.status(200).send({ gdiUrl: 'http://localhost:3000/3ds-server/frame.html' });
     }
 });
 
